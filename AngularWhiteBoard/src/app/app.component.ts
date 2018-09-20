745b1ff742f1e8e3b072a86f8a65855d2532f433
DESKTOP-M3T0SOU\tharu <tharushashehan@gmail.com>
2018-09-20 22:01:24 +0530

    new updates

--- AngularWhiteBoard/src/app/app.component.ts
+++ AngularWhiteBoard/src/app/app.component.ts
@@ -1,139 +1,34 @@
 import { Component } from '@angular/core';
+import { CanvasWhiteboardComponent, CanvasWhiteboardModule, CanvasWhiteboardOptions } from 'ng2-canvas-whiteboard';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+   viewProviders: [CanvasWhiteboardComponent]
 })
 export class AppComponent {
   title = 'AngularWhiteBoard';
   
- color; //to change line color
- width; //to change line width
- isPensil = false;
-
-directive("drawing", function(){
-  return {
-    restrict: "EA",
-    link: function(scope, element){
-
-      
-      var ctx = element[0].getContext('2d');
-      
-         console.log('asdasd'+element);
-      
-      // variable that decides if something should be drawn on mousemove
-      var drawing = false;
-      
-      // the last coordinates before the current move
-      var lastX;
-      var lastY;
-      
-      
-      element.bind('mousedown', function(event){
-        if(event.offsetX!==undefined){
-          lastX = event.offsetX;
-          lastY = event.offsetY;
-        } else {
-          lastX = event.layerX - event.currentTarget.offsetLeft;
-          lastY = event.layerY - event.currentTarget.offsetTop;
-        }
-        
-        // begins new line
-        ctx.beginPath();
-        
-        drawing = true;
-      });
-      element.bind('mousemove', function(event){
-        if(drawing && isPensil){
-          // get current mouse position
-          if(event.offsetX!==undefined){
-            currentX = event.offsetX;
-            currentY = event.offsetY;
-          } else {
-            currentX = event.layerX - event.currentTarget.offsetLeft;
-            currentY = event.layerY - event.currentTarget.offsetTop;
-          }
-          
-          draw(lastX, lastY, currentX, currentY);
-          
-          // set current coordinates to last one
-          lastX = currentX;
-          lastY = currentY;
-        }
-        
-      });
-      element.bind('mouseup', function(event){
-        // stop drawing
-        drawing = false;
-      });
-        
-      // canvas reset
-      function reset(){
-       element[0].width = element[0].width; 
-      }
-      
-      function draw(lX, lY, cX, cY){
-        // line from
-        console.log(lX,lY,cX,cY);
-        ctx.moveTo(lX,lY);
-        // to
-        ctx.lineTo(cX,cY);
-        // color
-        ctx.strokeStyle = color;
-        
-        ctx.lineWidth = width;
-        
-        // draw it
-        ctx.stroke();
-      }
-    //}
-  }
+canvasOptions: CanvasWhiteboardOptions = {
+    drawButtonEnabled: true,
+    drawButtonClass: "drawButtonClass",
+    drawButtonText: "Draw",
+    clearButtonEnabled: true,
+    clearButtonClass: "clearButtonClass",
+    clearButtonText: "Clear",
+    undoButtonText: "Undo",
+    undoButtonEnabled: true,
+    redoButtonText: "Redo",
+    redoButtonEnabled: true,
+    colorPickerEnabled: true,
+    saveDataButtonEnabled: true,
+    saveDataButtonText: "Save",
+    lineWidth: 5,
+    strokeColor: "rgb(0,0,0)",
+    shouldDownloadDrawing: true
   };
-});
-
-
-//controller for pencil tool
-app.controller('colorCtrl',function($scope){
-
-  //for change color
-  $scope.GetColor = function (selected) {
-                color = $scope.selected;
-                console.log('color is ' + color);
-                
-            }
-
-  //for change size
-  $scope.GetWidth = function (selected) {
-    width = $scope.selected;
-    console.log('width is ' + width);
-
-  }
-
-});
-
-controller('pencliCtrl',function($scope) {
-  $scope.getPencil = function()
-  {
-    isPensil = true;
-    console.log('isPensil=true');
-  }
-
-});
-
-// app.controller('ctrlNav', function () {});
-
-// app.directive("offCanvasMenu", function () {
-//     return {
-//         restrict: 'A',
-//         replace: false,
-//         link: function (scope, element) {
-//             scope.isMenuOpen = false;
-//             scope.toggleMenu = function () {
-//                 scope.isMenuOpen = !scope.isMenuOpen;
-//             };
-//         }
-//     };
-// });
-
+  
 }
--- AngularWhiteBoard/src/app/app.module.ts
+++ AngularWhiteBoard/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CanvasWhiteboardModule } from 'ng2-canvas-whiteboard';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CanvasWhiteboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
